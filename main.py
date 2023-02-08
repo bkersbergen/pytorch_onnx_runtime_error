9be19c3ec290e576241b5b7e867a7945440662b1
bkersbergen <bkersbergen@bol.com>
2023-02-08 09:35:40 +0100

    small reproducable error

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 import onnx
 import onnxruntime as ort
 import torch
@@ -6,70 +5,27 @@ from torch import nn
 
 
 class MyModel(nn.Module):
-    def __init__(self, embedding_size: int = 64, n_heads: int = 2, n_items: int = 5):
+    def __init__(self):
         super(MyModel, self).__init__()
-        self.item_embedding = nn.Embedding(n_items, embedding_size, padding_idx=0)
+        self.module = MultiHeadAttention()
 
-        self.trm_encoder = MultiHeadAttention(
-            n_heads=n_heads,
-            hidden_size=embedding_size,
-        )
-
-    def forward(self, item_seq):
-        attention_mask = item_seq.ne(0)
-        extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)  # torch.bool
-        expand_attention_mask = extended_attention_mask.expand((-1, -1, item_seq.size(-1), -1))
-        extended_attention_mask = torch.tril(expand_attention_mask)
-        extended_attention_mask = torch.where(extended_attention_mask, 0.0, -10000.0)
-
-        item_emb = self.item_embedding(item_seq)
-        alpha = self.trm_encoder(
-            item_emb, extended_attention_mask
-        ).to(torch.double)
-        return alpha
+    def forward(self, _item_seq):
+        mask = torch.rand((1, 1, 2, 2)) < 0.5
+        tril_mask = torch.tril(mask)
+        return self.module(tril_mask)
 
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, n_heads, hidden_size):
+    def __init__(self):
         super(MultiHeadAttention, self).__init__()
-        self.num_attention_heads = n_heads
-        self.attention_head_size = int(hidden_size / n_heads)
-        self.all_head_size = self.num_attention_heads * self.attention_head_size
-        self.sqrt_attention_head_size = math.sqrt(self.attention_head_size)
 
-        self.query = nn.Linear(hidden_size, self.all_head_size)
-        self.key = nn.Linear(hidden_size, self.all_head_size)
-        self.value = nn.Linear(hidden_size, self.all_head_size)
-
-    def transpose_for_scores(self, x):
-        new_x_shape = x.size()[:-1] + (
-            self.num_attention_heads,
-            self.attention_head_size,
-        )
-        x = x.view(*new_x_shape)
-        return x
-
-    def forward(self, input_tensor, attention_mask):
-        mixed_query_layer = self.query(input_tensor)
-        mixed_key_layer = self.key(input_tensor)
-        mixed_value_layer = self.value(input_tensor)
-
-        query_layer = self.transpose_for_scores(mixed_query_layer).permute(0, 2, 1, 3)
-        key_layer = self.transpose_for_scores(mixed_key_layer).permute(0, 2, 3, 1)
-        value_layer = self.transpose_for_scores(mixed_value_layer).permute(0, 2, 1, 3)
-
+    def forward(self, attention_mask):
+        query_layer = torch.rand((1, 2, 2, 32))
+        key_layer = torch.rand((1, 2, 32, 2))
         attention_scores = torch.matmul(query_layer, key_layer)
-
-        attention_scores = attention_scores / self.sqrt_attention_head_size
         attention_scores = attention_scores + attention_mask
-
-        context_layer = torch.matmul(attention_scores.to(torch.double), value_layer.to(torch.double)).to(torch.float32)
-        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
-        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-        context_layer = context_layer.view(*new_context_layer_shape)
-
-        return context_layer
+        return attention_scores
 
 
 if __name__ == '__main__':
